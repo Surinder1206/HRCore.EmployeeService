eec8124527604c670496c2160af104b16b8c922b
Surinder1206 <surinder.singh1206@gmail.com>
2025-07-15 11:27:53 +0100

    test: add failing controller test to verify service call on CreateEmployeeAsync
    - Ensures EmployeesController calls IEmployeeAppService.CreateAsync
    - Uses mock to verify the method is invoked once

---
+++ HRCore.EmpoyeeService/HRCore.EmployeeService.Tests/API/Controller/EmployeesControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using HRCore.EmployeeService.Application.DTOs;
+using HRCore.EmployeeService.Application.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace HRCore.EmployeeService.Tests.API.Controller;
+
+internal class EmployeesControllerTest
+{
+    [Test]
+    public async Task CreateEmployeeAsync_should_call_service_when_attempting_to_create_app()
+    {
+        // Arrange
+        var employeeServiceMock = new Mock<IEmployeeAppService>();
+        var controller = new EmployeesController(employeeServiceMock.Object);
+
+        // Act
+        await controller.CreateEmployeeAsync(new CreateEmployeeRequest());
+
+        // Assert
+        employeeServiceMock.Verify(s => s.CreateAsync(It.IsAny<EmployeeDto>()), Times.Once);
+    }
+}
