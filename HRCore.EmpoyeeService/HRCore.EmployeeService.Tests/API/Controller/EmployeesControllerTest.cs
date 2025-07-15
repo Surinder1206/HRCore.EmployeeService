5d615b5c8476eb4e16784d039d7e3340ce8dc679
Surinder1206 <surinder.singh1206@gmail.com>
2025-07-15 14:46:27 +0100

    test: add controller test to verify CreatedAtActionResult on successful employee creation
    - Ensures CreateEmployeeAsync returns 201 Created
    - Verifies response includes correct action name ("GetById")
    - Uses mocked service to simulate successful creation via ServiceResult

--- HRCore.EmpoyeeService/HRCore.EmployeeService.Tests/API/Controller/EmployeesControllerTest.cs
+++ HRCore.EmpoyeeService/HRCore.EmployeeService.Tests/API/Controller/EmployeesControllerTest.cs
@@ -1,7 +1,10 @@
-﻿using HRCore.EmployeeService.Application.DTOs;
+﻿using FluentAssertions;
+using HRCore.EmployeeService.Application.DTOs;
 using HRCore.EmployeeService.Application.Interfaces;
+using HRCore.EmployeeService.Application.Results;
 using HRCore.EmpoyeeService.API;
 using HRCore.EmpoyeeService.API.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 
@@ -21,5 +24,25 @@ internal class EmployeesControllerTest
 
         // Assert
         employeeServiceMock.Verify(s => s.CreateAsync(It.IsAny<EmployeeDto>()), Times.Once);
+    }
+
+    [Test]
+    public async Task CreateEmployeeAsync_should_return_201_Created_with_location_when_employee_creation_is_successful()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var employeeServiceMock = new Mock<IEmployeeAppService>();
+        var _employeeDto = ServiceResult.Success(new EmployeeDto() { Id = id });
+        employeeServiceMock.Setup(s => s.CreateAsync(It.IsAny<EmployeeDto>())).ReturnsAsync(_employeeDto);
+
+        var controller = new EmployeesController(employeeServiceMock.Object);
+
+        // Act 
+        var result = await controller.CreateEmployeeAsync(new CreateEmployeeRequest());
+
+        //
+        result.Should().BeOfType<CreatedAtActionResult>().Which.StatusCode.Should().Be(201);
+        result.Should().BeOfType<CreatedAtActionResult>().Which.ActionName.Should().Be("GetById");
+
     }
 }
